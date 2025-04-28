2898aac6bb646407fe576a745d76a05cacb23e11
Zair Montoya <z.montoya@uniandes.edu.co>
2025-04-28 08:47:42 -0500

    a

---
+++ src/app/asesoria/asesoria.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AsesoriaComponent } from './asesoria.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AsesoriaComponent]
+})
+export class AsesoriaModule { }
