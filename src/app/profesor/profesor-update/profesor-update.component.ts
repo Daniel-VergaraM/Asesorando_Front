f529d3796a0c9a7f94d780b2695a369ca4fb0ef6
Juan Manuel Rojas <jm.rojasp1@uniandes.edu.co>
2025-05-04 18:31:53 -0500

    update profesor terminado

---
+++ src/app/profesor/profesor-update/profesor-update.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProfesorService } from '../profesor.service';
+import { Profesor } from '../profesor';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  standalone: true,
+  selector: 'app-profesor-update',
+  templateUrl: './profesor-update.component.html',
+  styleUrls: ['./profesor-update.component.css'],
+  imports: [ReactiveFormsModule]
+})
+export class ProfesorUpdateComponent implements OnInit {
+  profesorForm!: FormGroup;
+  profesorId!: number;
+  profesorOriginal!: Profesor;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private profesorService: ProfesorService
+  ) {
+    this.profesorForm = this.fb.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      contrasena: ['', Validators.required],
+      telefono: ['', [Validators.required, Validators.minLength(7)]],
+      fotoUrl: [''],
+      videoUrl: [''],
+      tipo: ['VIRTUAL', Validators.required],
+      formacion: ['', Validators.required],
+      experiencia: ['', Validators.required],
+      enlaceReunion: [''],
+      codigoPostal: [''],
+      latitud: [''],
+      longitud: ['']
+    });
+  }
+
+  ngOnInit(): void {
+    this.profesorId = +this.route.snapshot.paramMap.get('id')!;
+    this.profesorService.getProfesor(this.profesorId).subscribe({
+      next: (p: Profesor) => {
+        this.profesorOriginal = p;
+        this.profesorForm.patchValue({
+          ...p,
+          codigoPostal: p.codigoPostal?.toString() || '',
+          latitud: p.latitud?.toString() || '',
+          longitud: p.longitud?.toString() || ''
+        });
+      },
+      error: err => console.error('Error al cargar profesor:', err)
+    });
+  }
+
+  onSubmitUpdate(): void {
+    const formValue = this.profesorForm.value;
+    const payload: Profesor = {
+      ...this.profesorOriginal,
+      ...formValue,
+      codigoPostal: formValue.codigoPostal ? +formValue.codigoPostal : null,
+      latitud: formValue.latitud ? +formValue.latitud : null,
+      longitud: formValue.longitud ? +formValue.longitud : null
+    };
+
+    console.log('Payload que mando:', payload);
+
+    this.profesorService.updateProfesor(payload).subscribe({
+      next: () => this.router.navigate(['/profesores']),
+      error: err => console.error('Error al actualizar profesor:', err)
+    });
+  }
+
+  cancelarUpdate(): void {
+    this.router.navigate(['/profesores']);
+  }
+}
