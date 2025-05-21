7e5ce90faad2bb9bc42594625e32c73628ff2790
Victoria González <vs.gonzalezc1@uniandes.edu.co>
2025-05-21 10:21:02 -0500

    Commit

--- src/app/asignaciones/asignaciones.component.spec.ts
+++ src/app/asignaciones/asignaciones.component.spec.ts
@@ -1,3 +1,5 @@
+/// <reference types="jasmine" />
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AsignacionesComponent } from './asignaciones.component';
 
@@ -12,9 +14,10 @@ describe('AsignacionesComponent', () => {
 
     fixture = TestBed.createComponent(AsignacionesComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  expect(component).toBeTruthy();
+});
 });
